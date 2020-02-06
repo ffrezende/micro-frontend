8ab5daea848d57f44fd926ca162bb2d035a6e323
Fabricio Fraga <root@lnb024490bhz.ciandt.global>
2020-02-06 17:20:44 -0300

    g

--- src/app1/index.js
+++ src/app1/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import singleSpaReact from 'single-spa-react';
-import Root from './root.component.tsx';
+import Root from './pages/root.component.tsx';
 
 const reactLifecycles = singleSpaReact({
   React,
